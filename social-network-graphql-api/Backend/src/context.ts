0ba452eb29073842ceac06e61a0da3f5839649a3
Youba.I <mevshypocrisie@yahoo.com>
2024-05-23 21:19:56 +0200

    ✨Introduce new feature

--- social-network-graphql-api/Backend/src/context.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-import { JWTUser } from './module/auth.module.js';
-
-export type DataSourceContext = {
-    dataSources: {
-        client: PrismaClient;
-    };
-    user: JWTUser | null;
-};
